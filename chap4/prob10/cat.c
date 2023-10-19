f992b5428481b2a0d143d1a8269f8c9ef3064f36
leemiju <dlalwnjenny@naver.com>
2023-10-19 04:26:22 +0000

    test

--- chap4/prob10/cat.c
+++ chap4/prob10/cat.c
@@ -2,45 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 int main(int argc, char *argv[]) {
-    int line_number = 0;
+    int line_number = 1;
     FILE *fp;
-    int c;
+    char line[1024];
+    int print_line_numbers = 0;
 
     if (argc < 2) {
-        printf(": %s\n", argv[0]);
+        printf("%s\n", argv[0]);
         return 1;
     }
 
     int arg_index = 1;
-    int print_line_numbers = 0;
 
     if (strcmp(argv[1], "-n") == 0) {
         print_line_numbers = 1;
         arg_index = 2;
     }
 
-  
+
     for (; arg_index < argc; arg_index++) {
         fp = fopen(argv[arg_index], "r");
         if (fp == NULL) {
-            fprintf(stderr, ": %s\n", argv[arg_index]);
+            fprintf(stderr,"%s\n", argv[arg_index]);
             continue;
         }
 
-        
 
-        c = getc(fp);
-        while (c != EOF) {
+      while (fgets(line, sizeof(line), fp) != NULL) {
             if (print_line_numbers) {
-                printf("%d: ", ++line_number);
+                printf("%d ", line_number);
             }
-            putc(c, stdout);
-            if (c == '\n') {
-                line_number++;
-            }
-            c = getc(fp);
+            printf("%s", line);
+            line_number++;
         }
 
         fclose(fp);
@@ -49,3 +43,4 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+ 
