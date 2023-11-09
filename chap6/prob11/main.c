147973e71894162ea2b30962e17b2f9a14288b0c
leemiju <dlalwnjenny@naver.com>
2023-11-09 04:01:12 +0000

    test

--- chap6/prob11/main.c
+++ chap6/prob11/main.c
@@ -1,87 +1,121 @@
-  #include <sys/types.h>
-  #include <sys/stat.h>
-   #include <dirent.h>
- #include <stdio.h>
- #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <grp.h>
+#include <pwd.h>
+#include <string.h>
 
-  #include <time.h>
-  #include <grp.h>
-  #include <pwd.h>
-  #include <string.h> 
+void printStat(char *, char *, struct stat *, char *);
+char type(mode_t);
+char *perm(mode_t);
 
- char type(mode_t);
- char *perm(mode_t);
- void printStat(char*, char*, struct stat*);
- int main(int argc, char **argv)
- {
-     DIR *dp;
-     char *dir;
-     struct stat st;
-     struct dirent *d;
-     char path[BUFSIZ+1];
-     if (argc == 1)
-         dir = ".";
-     else dir = argv[1];
+int main(int argc, char **argv)
+{
+    DIR *dp;
+    char *dir;
+    struct stat st;
+    struct dirent *d;
+    char path[BUFSIZ + 1];
 
-     if ((dp = opendir(dir)) == NULL) 
-         perror(dir);
+    char *options = "";
+    if (argc >= 3 && argv[1][0] == '-')
+    {
+        options = argv[1] + 1;
+        dir = argc == 3 ? "." : argv[3]; 
+    }
+    else
+    {
+        dir = argc == 1 ? "." : argv[1];
+    }
 
-     while ((d = readdir(dp)) != NULL) { 
-         sprintf(path, "%s/%s", dir, d -> d_name);          
-        if (lstat(path, &st) < 0) 
-         perror(path);
-         else 
-             printStat(path, d->d_name, &st);
-     }
+    if ((dp = opendir(dir)) == NULL)
+        perror(dir);
 
-     closedir(dp);
-     exit(0);
- }
+    while ((d = readdir(dp)) != NULL)
+    {
+        sprintf(path, "%s/%s", dir, d->d_name);
+        if (lstat(path, &st) < 0)
+            perror(path);
+        else
+        {
+            printStat(path, d->d_name, &st, options);
+        }
+    }
 
+    closedir(dp);
+    exit(0);
+}
 
-void printStat(char *pathname, char *file, struct stat *st) 
- {
-     printf("%5d ", st->st_blocks);
-     printf("%c%s ", type(st->st_mode), perm(st->st_mode));
-     printf("%3d ", st->st_nlink);
-    printf("%s %s ", getpwuid(st->st_uid)->pw_name,
-                 getgrgid(st->st_gid)->gr_name);
-     printf("%9d ", st->st_size);
-   printf("%.12s ", ctime(&st->st_mtime)+4);
-     printf("%s\n", file);
- }
+void printStat(char *pathname, char *file, struct stat *st, char *options)
+{
+   
+    for (int i = 0; i < strlen(options); i++)
+    {
+        switch (options[i])
+        {
+        case 'p':
+            printf("%s ", strcmp(file, ".") == 0 ? "./" : (strcmp(file, "..") == 0 ? "../" : file));
+            break;
+        case 'i':
+            printf("%5d ", st->st_ino);
+            break;
+        case 'Q':
+            printf("\"%s\" ", strcmp(file, ".") == 0 ? "." : (strcmp(file, "..") == 0 ? ".." : file));
+            break;
+        default:
+       
+            fprintf(stderr, "Unsupported option: -%c\n", options[i]);
+            exit(1);
+        }
+    }
 
- char type(mode_t mode) 
- {
-    if (S_ISREG(mode))
-       return('-');
-    if (S_ISDIR(mode))
-        return('d');
-    if (S_ISCHR(mode))
-        return('c');
-    if (S_ISBLK(mode))
-        return('b');
-    if (S_ISLNK(mode))
-         return('l');
-     if (S_ISFIFO(mode))
-        return('p');
-    if (S_ISSOCK(mode))
-        return('s');
- }
-
- char* perm(mode_t mode) 
- {
-     static char perms[10];
-     strcpy(perms, "---------");
  
-     for (int i=0; i < 3; i++) {
-         if (mode & (S_IRUSR >> i*3))
-             perms[i*3] = 'r';
-         if (mode & (S_IWUSR >> i*3))
-            perms[i*3+1] = 'w';
-         if (mode & (S_IXUSR >> i*3))
-            perms[i*3+2] = 'x';
-     }
-     return(perms);
- }
+    printf("%c%s ", type(st->st_mode), perm(st->st_mode));
+    printf("%3d ", st->st_nlink);
+    printf("%s %s ", getpwuid(st->st_uid)->pw_name,
+           getgrgid(st->st_gid)->gr_name);
+    printf("%9d ", st->st_size);
+    printf("%.12s ", ctime(&st->st_mtime) + 4);
+    printf("%s\n", file);
+}
+
+char type(mode_t mode)
+{
+    if (S_ISREG(mode))
+        return '-';
+    if (S_ISDIR(mode))
+        return 'd';
+    if (S_ISCHR(mode))
+        return 'c';
+    if (S_ISBLK(mode))
+        return 'b';
+    if (S_ISLNK(mode))
+        return 'l';
+    if (S_ISFIFO(mode))
+        return 'p';
+    if (S_ISSOCK(mode))
+        return 's';
+
+    return '?';
+}
+
+char *perm(mode_t mode)
+{
+    static char perms[10];
+    strcpy(perms, "---------");
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (mode & (S_IRUSR >> i * 3))
+            perms[i * 3] = 'r';
+        if (mode & (S_IWUSR >> i * 3))
+            perms[i * 3 + 1] = 'w';
+        if (mode & (S_IXUSR >> i * 3))
+            perms[i * 3 + 2] = 'x';
+    }
+    return (perms);
+}
 
