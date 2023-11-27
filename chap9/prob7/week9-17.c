6b3c1fd1018c3e2bb17b57416df940d713f5f169
leemiju <dlalwnjenny@naver.com>
2023-11-27 02:24:39 +0000

    test

---
+++ chap9/prob7/week9-17.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_ARG_LENGTH 100
+#define MAX_INPUT_LENGTH 1024
+
+int main() {
+    char input[MAX_INPUT_LENGTH];
+    char *token;
+    char *args[MAX_ARG_LENGTH];
+    int background;
+
+    while (1) {
+        printf("Pls input cmd : ");
+        fflush(stdout);
+
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            perror("Error reading input");
+            exit(EXIT_FAILURE);
+        }
+
+        input[strcspn(input, "\n")] = '\0';
+
+        if (strlen(input) == 0)
+            continue;
+
+        background = 0;
+        if (input[strlen(input) - 1] == '&') {
+            background = 1;
+            input[strlen(input) - 1] = '\0';
+        }
+
+        token = strtok(input, " ");
+        int i = 0;
+
+        while (token != NULL) {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        args[i] = NULL;
+
+        pid_t pid = fork();
+
+        if (pid == 0) {
+            if (execvp(args[0], args) == -1) {
+                perror("Execution failed");
+                exit(EXIT_FAILURE);
+            }
+        } else if (pid > 0) {
+            if (!background) {
+                int status;
+                waitpid(pid, &status, 0);
+            }
+        } else {
+            perror("Fork failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
+}
+
